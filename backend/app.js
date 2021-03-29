02e4d1c2a34a8fd65a093b718b983f230617746a
babyazalea <usun12@hotmail.com>
2021-03-29 21:04:16 +0900

    making add-post request

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -15,7 +18,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/posts", (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully",
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
   const posts = [
     { id: "a12345", title: "First server-side post", content: "backend test" },
     { id: "b12345", title: "Second server-side post", content: "backend test" },
