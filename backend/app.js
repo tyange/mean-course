edcf45fd1c14be2735d6038d7ac3dd82f45be05f
babyazalea <usun12@hotmail.com>
2021-04-01 23:52:32 +0900

    modified post request in backend for connecting to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const Post = require("./models/post");
+
 const app = express();
 
 app.use(express.json());
@@ -19,7 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully",
