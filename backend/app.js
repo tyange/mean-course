b05b2c9b7432f257a5db77a7cdf92cdac27cf381
babyazalea <usun12@hotmail.com>
2021-04-03 23:35:33 +0900

    fetching data from mongodb-database

--- backend/app.js
+++ backend/app.js
@@ -40,20 +40,18 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added successfully",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    { id: "a12345", title: "First server-side post", content: "backend test" },
-    { id: "b12345", title: "Second server-side post", content: "backend test" },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
   });
 });
 
