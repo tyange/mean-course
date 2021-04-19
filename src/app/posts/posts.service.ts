41107a5989b3858fa8c8d9f7c1c02924ace6d5b3
babyazalea <usun12@hotmail.com>
2021-04-19 16:24:09 +0900

    added loading spinner

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,13 +4,14 @@ import { Subject } from 'rxjs';
 import {map} from 'rxjs/operators'
 
 import { Post } from './post.model';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private rotuer: Router) {}
 
   getPosts() {
     // return [...this.posts];
@@ -50,6 +51,7 @@ export class PostsService {
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.rotuer.navigate(["/"])
       });
   }
 
@@ -63,6 +65,7 @@ export class PostsService {
       updatedPosts[oldPostIndex] = post;
       this.posts = updatedPosts;
       this.postsUpdated.next([...this.posts]);
+      this.rotuer.navigate(["/"])
     });
   }
 
